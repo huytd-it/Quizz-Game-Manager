02eedd51336aa19f64f1e4241cbf8bd0a651c2da
giangnho <54979798+giangnho@users.noreply.github.com>
2019-12-11 13:36:04 +0700

    Hoàn thành 100%

--- resources/views/ds_nguoi_choi.blade.php
+++ resources/views/ds_nguoi_choi.blade.php
@@ -56,6 +56,10 @@
                 <div class="modal-body">
                     <h4>Điểm</h4>
                     <p>{{$ctnc->diem}}</p>
+                    <h4>Số Credit đã mua </h4>
+                    <p{{$ctnc->credit}}></p>
+                    <h4>Tổng số tiền</h4>
+                    <p{{$ctnc->so_tien}}></p>
                     <hr>
                 </div>
                 <div class="modal-footer">
